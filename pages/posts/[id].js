cac49add2a06058f5a338f249b40a3d5279fb09e
kiana <kianazhi@tech04.com>
2022-12-07 17:57:13 +0800

    详情数据获取

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,64 @@
-import Layout from '../../components/layout';
+import Layout from '../../component/layout';
+// import { getAllPostIds, getPostData } from '../../lib/posts';
+import axios from "axios";
+import matter from "gray-matter";
+import { remark } from 'remark';
+import html from 'remark-html';
 
-export default function Post() {
-    return <Layout>...</Layout>;
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </Layout>
+    );
 }
 
 export async function getStaticPaths() {
-    // Return a list of possible value for id
+    // const paths = getAllPostIds();
+
+    const response = await axios({
+        method: 'get',
+        // responseType: 'stream',
+        url: "https://gitee.com/api/v5/repos/coffeegirl/test-data/git/trees/master?access_token=acd71d3f8fe43bcf58b556de87dca3fe"
+    });
+
+    const paths = response.data.tree.map((ele) => {
+        return {
+            params: {
+                // id: ele.path.replace(/\.md$/, ''),
+                id: ele.sha
+                // sha: ele.sha
+            },
+        }
+    })
+    console.log(paths);
+
+
+    return {
+        paths,
+        fallback: false,
+    };
 }
 
 export async function getStaticProps({ params }) {
-    // Fetch necessary data for the blog post using params.id
+    // Add the "await" keyword like this:
+    console.log(params.id, 'params');
+    // const postData = await getPostData(params.sha);
+    const res = await axios({
+        method: 'get',
+        // responseType: 'stream',
+        url: `https://gitee.com/api/v5/repos/coffeegirl/test-data/git/blobs/${params.id}?access_token=acd71d3f8fe43bcf58b556de87dca3fe`
+    });
+    // return {
+    //     props: {
+    //         postData,
+    //     },
+    // };
 }
